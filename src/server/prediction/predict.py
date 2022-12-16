2b82d2695e83a49b1f6f77b278fc1636fdff2af2
Ryan <ryanwallace1396@gmail.com>
2022-12-16 15:19:59 -0800

    finished server

--- src/server/prediction/predict.py
+++ src/server/prediction/predict.py
@@ -7,19 +7,21 @@ import pickle
 def transformData(data):
     df = pd.read_csv('./data/rowPrediction.csv')
 
+    print(df.loc[0, 'ConditionAAA'])
+
     for key, val in data.items():
-        if val == 'noDiamond' or val == 'ConditionAAA':
+
+        if val == 'noDiamond':
             continue
         else:
             df.loc[0, val] = 1
-
     # print(df.loc[0, :])
 
     return makePrediction(df)
 
 def makePrediction(row):
     # load in the model and make a prediction on the row
-    model = pickle.load(open('./model/watches_lgbm_initial_model.pkl', 'rb'))
+    model = pickle.load(open('./model/lgbmmodel.pkl', 'rb'))
     
     # print("PREDICTING!!")
     # check which columns in the rowPrediction are not in 
@@ -27,18 +29,13 @@ def makePrediction(row):
     #     if col not in model.feature_name_:
     #         print("NOT IN: ", col)
     prediction = model.predict(row)
-    
-    # print("Prediction: ", prediction)
-    print(model.feature_importances_)
-    
-    fImportances = featImportance(row, model.feature_importances_)
 
     # prediction is stored in the first element of a list
     return round(prediction[0], 2)
 
 # determine the top 5 features
-def featImportance(row, importances):
-    pass
+# def featImportance(row, importances):
+#     pass
 
 if __name__ == '__main__':
     transformData([])
--- src/server/server.py
+++ src/server/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify, Response, render_template
 import pickle
 from prediction.predict import transformData
-
+import os
 
 app = Flask(__name__)
-list_params = ["watch-model", "case-material", "strap-type", "dial-type", "case-size"]
-
+# list_params = ["watch-model", "case-material", "strap-type", "dial-type", "case-size"]
+# make a directory to store images that will be rendered to browser
+imageFolder = os.path.join('static', 'images')
+app.config['UPLOAD_FOLDER'] = imageFolder
 
 @app.route("/", methods=['GET'])
 def index():
@@ -21,7 +23,10 @@ def output_quote():
     # reformat into dataframe row and make prediction
     prediction = transformData(quote)
 
-    return render_template('quote.html', prediction=prediction, watchName=quote['watch-model'])
+    # get feature images to render in html
+    featureGraph = os.path.join(app.config['UPLOAD_FOLDER'], 'featureGraph.png')
+
+    return render_template('quote.html', prediction=prediction, watchName=quote['watch-model'], featureGraph=featureGraph)
 
 if __name__ == '__main__':
     # run() method of Flask class runs the application
